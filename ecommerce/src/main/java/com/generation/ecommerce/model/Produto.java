0a94d2f817238166fcea45864b81c6a1e78a7d33
Marcelo Barboza <marcelo.barboza.cmkp@gmail.com>
2020-03-16 13:11:51 -0300

    Adicionando dados do passo 1

---
+++ ecommerce/src/main/java/com/generation/ecommerce/model/Produto.java
@@ -0,0 +1,65 @@
+package com.generation.ecommerce.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+
+import com.sun.istack.NotNull;
+
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 5, max = 256)
+	private String nome;
+	
+	@Min(0)
+	private int qtdStoque;
+	
+	private BigDecimal valor;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getQtdStoque() {
+		return qtdStoque;
+	}
+
+	public void setQtdStoque(int qtdStoque) {
+		this.qtdStoque = qtdStoque;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+	
+}
